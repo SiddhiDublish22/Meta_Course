61ecb9af3b3c3639feafa51a08e0a42152534cec
SiddhiDublish22 <siddhidublish.22@gmail.com>
2025-08-03 22:50:29 +0530

    Create App.js

---
+++ little-lemon-booking/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BookingForm from "./components/BookingForm";
+
+function App() {
+  return (
+    <main>
+      <h1>Reserve a Table at Little Lemon</h1>
+      <BookingForm />
+    </main>
+  );
+}
+
+export default App;
